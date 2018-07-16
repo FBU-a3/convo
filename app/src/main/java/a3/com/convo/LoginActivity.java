b48d4891052268cab6cab1426f3e8dc9dc5b6a7a
Alveera Khan <alveera@fb.com>
2018-07-16 16:46:41 -0700

    layout login screen

--- app/src/main/java/a3/com/convo/LoginActivity.java
+++ app/src/main/java/a3/com/convo/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -11,9 +12,14 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -39,6 +45,7 @@ public class LoginActivity extends AppCompatActivity {
                 AccessToken accessToken = AccessToken.getCurrentAccessToken();
                 boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
                 Toast.makeText(context, "Logged in successfully", Toast.LENGTH_LONG).show();
+                getLikedPageInfo(loginResult);
             }
 
             @Override
@@ -65,5 +72,42 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    protected void getLikedPageInfo(LoginResult login_result) {
+        GraphRequest data_request = GraphRequest.newMeRequest(
+                login_result.getAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(
+                            JSONObject json_object,
+                            GraphResponse response) {
+
+                        try {
+                            // convert Json object into Json array
+                            JSONArray posts = json_object.getJSONObject("likes").optJSONArray("data");
+
+                            for (int i = 0; i < posts.length(); i++) {
+
+                                JSONObject post = posts.optJSONObject(i);
+                                String id = post.optString("id");
+                                String category = post.optString("category");
+                                String name = post.optString("name");
+                                int count = post.optInt("likes");
+                                // print id, page name and number of like of facebook page
+                                Log.e("id -", id+" name -"+name+ " category-"+
+                                        category+ " likes count -" + count);
+                            }
+
+                        } catch(Exception e){
+
+                        }
+                    }
+                });
+        Bundle permission_param = new Bundle();
+        // add the field to get the details of liked pages
+        permission_param.putString("fields", "likes{id,category,name,location,likes}");
+        data_request.setParameters(permission_param);
+        data_request.executeAsync();
     }
 }
