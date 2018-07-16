437caa595b2ef5717927d9b9819fb1b26809fe04
Alex Goodman <alexgood@stanford.edu>
2018-07-16 10:51:37 -0700

    started setup of Facebook SDK integration

---
+++ app/src/main/java/a3/com/convo/LoginActivity.java
@@ -0,0 +1,42 @@
+package a3.com.convo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        CallbackManager callbackManager = CallbackManager.Factory.create();
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+    }
+}
