418b125ce5de4bf1ea204c86149b743d913dbdbe
Alveera Khan <alveera@fb.com>
2018-07-16 13:57:18 -0700

    added onClick

--- app/src/main/java/a3/com/convo/LoginActivity.java
+++ app/src/main/java/a3/com/convo/LoginActivity.java
@@ -1,25 +1,34 @@
 package a3.com.convo;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
     LoginButton loginButton;
     CallbackManager callbackManager;
+    Activity context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        context = this;
 
         loginButton = (LoginButton) findViewById(R.id.login_button);
         callbackManager = CallbackManager.Factory.create();
@@ -39,6 +48,16 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onError(FacebookException exception) {
                 // App code
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                LoginManager.getInstance().logInWithReadPermissions(context, Arrays.asList("public_profile"));
+                Toast.makeText(context, "Logged in successfully", Toast.LENGTH_LONG);
             }
         });
     }
