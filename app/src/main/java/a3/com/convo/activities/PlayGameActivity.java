e2670d9e5ec5d7459ea1a9d80c259384e357f886
Alex Goodman <alexgood@stanford.edu>
2018-07-19 11:41:18 -0700

    added play game activity and fragment

---
+++ app/src/main/java/a3/com/convo/activities/PlayGameActivity.java
@@ -0,0 +1,15 @@
+package a3.com.convo.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import a3.com.convo.R;
+
+public class PlayGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_game);
+    }
+}
