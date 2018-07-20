b3f640bc07f7349896cabde6fbc813cdf8d346dc
Andrea Vieira <andreavieira@fb.com>
2018-07-20 10:30:26 -0700

    Set up friend list adapter.

--- app/src/main/java/a3/com/convo/adapters/FriendAdapter.java
+++ app/src/main/java/a3/com/convo/adapters/FriendAdapter.java
@@ -1,6 +1,7 @@
 package a3.com.convo.adapters;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,19 +9,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import a3.com.convo.GlideApp;
 import a3.com.convo.R;
+import a3.com.convo.models.User;
 
 public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder> {
-    private List<String> myFriends;
+    private ArrayList<String> myFriends;
     Context context;
 
-    public FriendAdapter(List<String> friends) {
+    public FriendAdapter(ArrayList<String> friends) {
         myFriends = friends;
     }
 
@@ -37,18 +48,46 @@ public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull FriendAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final FriendAdapter.ViewHolder holder, int position) {
         // Get friends user ID's
         String friendId = myFriends.get(position);
+        final String profilePicPath = "/" + friendId + "/picture";
 
         // Get friends name
-        holder.tvFriend.setText("Friend Name");
 
-        ParseFile profPic = ParseUser.getCurrentUser().getParseFile("profile");
-//        GlideApp.with(context)
-//                .load(profPic.getUrl())
-//                .circleCrop()
-//                .into(holder.ivFriend);
+        GraphRequest picRequest = GraphRequest.newGraphPathRequest(AccessToken.getCurrentAccessToken(), profilePicPath, new GraphRequest.Callback() {
+            @Override
+            public void onCompleted(GraphResponse response) {
+                JSONObject object = response.getJSONObject();
+                String picURL = null;
+                try {
+                    picURL = object.getString("url");
+                    GlideApp.with(context)
+                            .load(picURL)
+                            .circleCrop()
+                            .into(holder.ivFriend);
+                } catch (JSONException e) {
+                    Toast.makeText(context, "Pic URL not retrieved", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        Bundle picParams = new Bundle();
+        picParams.putString("fields", "url");
+        picRequest.setParameters(picParams);
+        picRequest.executeAsync();
+
+        GraphRequest nameRequest = GraphRequest.newGraphPathRequest(AccessToken.getCurrentAccessToken(), "/" + friendId, new GraphRequest.Callback() {
+            @Override
+            public void onCompleted(GraphResponse response) {
+                try {
+                    String name = response.getJSONObject().getString("name");
+                    holder.tvFriend.setText(name);
+                } catch (JSONException e) {
+                    Toast.makeText(context, "Name not retrieved", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        nameRequest.executeAsync();
     }
 
     @Override
@@ -72,5 +111,10 @@ public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder
         public void onClick(View view) {
 
         }
+    }
+
+    public void clear() {
+        myFriends.clear();
+        notifyDataSetChanged();
     }
 }
--- app/src/main/java/a3/com/convo/fragments/FriendsFragment.java
+++ app/src/main/java/a3/com/convo/fragments/FriendsFragment.java
@@ -3,17 +3,31 @@ package a3.com.convo.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import a3.com.convo.R;
 import a3.com.convo.activities.PlayGameActivity;
+import a3.com.convo.adapters.FriendAdapter;
+import a3.com.convo.models.User;
 
 public class FriendsFragment extends Fragment{
+    RecyclerView friendsRv;
     private Context context;
     private Button modeButton;
+    FriendAdapter friendAdapter;
+    ArrayList<String> friends;
 
     public FriendsFragment() {
         // Required empty public constructor
@@ -37,6 +51,18 @@ public class FriendsFragment extends Fragment{
                 ((PlayGameActivity)context).goToMode();
             }
         });
+        friends = new ArrayList<>();
+        friendsRv = view.findViewById(R.id.rv_friends);
+        friendAdapter = new FriendAdapter(friends);
+        friendsRv.setLayoutManager(new LinearLayoutManager(getActivity()));
+        friendsRv.setAdapter(friendAdapter);
 
+        //loadFriends();
+    }
+
+
+    public void fetchTimelineAsync(int page) {
+        friendAdapter.clear();
+        //loadFriends();
     }
 }
