6806e5f5b1e4ebb005ddf8c8ec50dd5c083489b3
Alex Goodman <alexgood@stanford.edu>
2018-07-27 15:50:37 -0700

    bug fix for image loading and getting to end of game w/ Andrea

--- app/src/main/java/a3/com/convo/fragments/GameFragment.java
+++ app/src/main/java/a3/com/convo/fragments/GameFragment.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
