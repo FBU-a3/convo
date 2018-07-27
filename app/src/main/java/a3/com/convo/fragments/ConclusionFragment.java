66fe2dfd9c5eb6250433d6f4b02f0baded3c462a
Alex Goodman <alexgood@stanford.edu>
2018-07-27 15:50:37 -0700

    bug fix for image loading and getting to end of game w/ Andrea

--- app/src/main/java/a3/com/convo/fragments/ConclusionFragment.java
+++ app/src/main/java/a3/com/convo/fragments/ConclusionFragment.java
@@ -3,16 +3,22 @@ package a3.com.convo.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import a3.com.convo.Constants;
 import a3.com.convo.R;
 import a3.com.convo.activities.PlayGameActivity;
 
 
 public class ConclusionFragment extends Fragment {
-    private String discussedTopics;
+    private ArrayList<String> discussedTopics;
 
     public ConclusionFragment() {
 
@@ -27,6 +33,10 @@ public class ConclusionFragment extends Fragment {
 
     public void onViewCreated(View view, Bundle savedInstanceState) {
         Button playAgainButton = (Button) view.findViewById(R.id.play_again_btn);
+        playAgainButton = (Button) view.findViewById(R.id.play_again_btn);
+        TextView tvTopics = (TextView) view.findViewById(R.id.tv_topics);
+
+        tvTopics.setText(TextUtils.join(", ", discussedTopics));
 
         playAgainButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +45,9 @@ public class ConclusionFragment extends Fragment {
                     ((PlayGameActivity) getContext()).goToFriends();
             }
         });
+    }
+
+    public void setDiscussedTopics(ArrayList<String> topicsDiscussed) {
+        discussedTopics = topicsDiscussed;
     }
 }
