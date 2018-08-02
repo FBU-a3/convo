36ceb7dec5f9d504348f7077f660ab22b42cd8de
Alveera Khan <alveera@fb.com>
2018-08-02 13:04:47 -0700

    improved login screen

--- app/src/main/java/a3/com/convo/activities/LoginActivity.java
+++ app/src/main/java/a3/com/convo/activities/LoginActivity.java
@@ -2,10 +2,13 @@ package a3.com.convo.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
@@ -14,6 +17,7 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.parse.FindCallback;
@@ -44,6 +48,7 @@ public class LoginActivity extends AppCompatActivity {
     // maps Page IDs to Object IDs for quick lookup of duplicate pages
     private HashMap<String, String> existingPages;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,6 +93,10 @@ public class LoginActivity extends AppCompatActivity {
                 Constants.USER_LOCATION,
                 Constants.USER_TAGGED_PLACES));
         mCallbackManager = CallbackManager.Factory.create();
+        loginButton.setScaleX((float)2);
+        loginButton.setScaleY((float)2);
+        loginButton.setBackground(getDrawable(R.drawable.login_button));
+
         final AccessToken accessToken = AccessToken.getCurrentAccessToken();
         final boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
 
