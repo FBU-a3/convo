59fe6500cb4587018be72fdc0f1a36dd34bb39b0
Andrea Vieira <andreavieira@fb.com>
2018-08-02 20:50:05 -0700

    Topic summary initiated.

--- app/src/main/java/a3/com/convo/fragments/ConclusionFragment.java
+++ app/src/main/java/a3/com/convo/fragments/ConclusionFragment.java
@@ -3,11 +3,16 @@ package a3.com.convo.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
 
@@ -19,7 +24,6 @@ public class ConclusionFragment extends Fragment {
     private ArrayList<String> discussedTopics;
 
     public ConclusionFragment() {
-
     }
 
     @Override
@@ -31,11 +35,25 @@ public class ConclusionFragment extends Fragment {
 
     public void onViewCreated(View view, Bundle savedInstanceState) {
         Button playAgainButton = (Button) view.findViewById(R.id.play_again_btn);
-        playAgainButton = (Button) view.findViewById(R.id.play_again_btn);
         TextView tvTopics = (TextView) view.findViewById(R.id.tv_topics);
 
-        tvTopics.setText(TextUtils.join(Constants.JOIN_STRING, discussedTopics));
+        ParseQuery<a3.com.convo.models.Page> query = ParseQuery.getQuery(a3.com.convo.models.Page.class);
+        query.getInBackground(objectId, new GetCallback<a3.com.convo.models.Page>() {
+            @Override
+            public void done(a3.com.convo.models.Page object, ParseException e) {
+                if (e == null && discussedTopics != null) {
+                    for (int i = 0; i < discussedTopics.size(); i++) {
+                        discussedTopics.set(i, object.getName());
+                    }
+                }
+                else {
+                        Log.e("Page name error", "Oops!");
+                        e.printStackTrace();
+                }
+            }
+        });
 
+        tvTopics.setText(TextUtils.join(Constants.JOIN_STRING, discussedTopics));
         playAgainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
