2c596a5e86380bca54c7a123463661005da4b826
Alex Goodman <alexgood@stanford.edu>
2018-08-07 15:42:17 -0700

    # This is a combination of 4 commits. # This is the 1st commit message:
    added dynamic background color to cards and standardized cover photo size
    
    # This is the commit message #2:
    
    Combined profile_details and HomeScreenActivity and added coverUrl to tagged_places
    
    # This is the commit message #3:
    
    Combined profile_details and HomeScreenActivity and added coverUrl to tagged_places
    
    # This is the commit message #4:
    
    Combined profile_details and HomeScreenActivity and added coverUrl to tagged_places

--- app/src/main/java/a3/com/convo/activities/ProfileActivity.java
+++ app/src/main/java/a3/com/convo/activities/ProfileActivity.java
@@ -1,14 +1,11 @@
 package a3.com.convo.activities;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import a3.com.convo.R;
 import a3.com.convo.fragments.AddInfoFragment;
-import a3.com.convo.fragments.ProfileDetailsFragment;
 
 public class ProfileActivity extends AppCompatActivity {
     private FragmentTransaction ft;
@@ -19,24 +16,7 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         ft = getSupportFragmentManager().beginTransaction();
-
-        ft.replace(R.id.show_profile_fragment, new ProfileDetailsFragment());
+        ft.replace(R.id.show_profile_fragment, new AddInfoFragment());
         ft.commit();
-    }
-
-    public void goToAddInfo() {
-        Fragment fragment = new AddInfoFragment();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.show_profile_fragment, fragment);
-        fragmentTransaction.commit();
-    }
-
-    public void goBackToProfile() {
-        Fragment fragment = new ProfileDetailsFragment();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.show_profile_fragment, fragment);
-        fragmentTransaction.commit();
     }
 }
